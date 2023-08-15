a3b0f36ca24f07be8db246ef8832af61c3177ea2
jt ciou <jtciou26@gmail.com>
2023-08-15 15:27:51 +0800

    test material name in gtm

---
+++ testMaterialName.js
@@ -0,0 +1,18 @@
+<script>
+
+(function(){
+   var interval = setInterval(function(){
+   var inputField = document.querySelector('input');
+    if (inputField != null) {
+      var mn = inputField.textContent = {{cjs - gets material name during creating classroom}};
+      console.log (mn);
+      inputField.value = mn;
+      inputField.setAttribute("value", mn);
+
+      clearInterval(interval);
+    }
+  },1000);
+  
+})();
+ 
+</script>
